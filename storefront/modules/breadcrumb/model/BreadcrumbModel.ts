c2ab7539ffe5372210992a30fbb79282e2335897
ITs-GiaHuy <giahuytechie@gmail.com>
2026-03-15 08:06:20 +0000

    Test CI

--- storefront/modules/breadcrumb/model/BreadcrumbModel.ts
+++ storefront/modules/breadcrumb/model/BreadcrumbModel.ts
@@ -3,4 +3,4 @@ export type BreadcrumbModel = {
   url: string;
 };
 
-// Test CI 10
+// Test CI 11
--- tax/src/main/java/com/yas/tax/service/AbstractCircuitBreakFallbackHandler.java
+++ tax/src/main/java/com/yas/tax/service/AbstractCircuitBreakFallbackHandler.java
@@ -20,4 +20,4 @@ abstract class AbstractCircuitBreakFallbackHandler {
     }
 }
 
-// Test CI 10
+// Test CI 11
